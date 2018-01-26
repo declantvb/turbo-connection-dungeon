c4c809be6452449f343e3a5ce083c71f3e011889
Declan ter Veer-Burke <declantvb@gmail.com>
2018-01-26 20:44:29 +1300

    socket test

--- game/index.js
+++ game/index.js
@@ -4,9 +4,19 @@ var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
 
-app.use(express.static(__dirname + '/bower_components'));
+app.use(express.static(__dirname + '/public'));
+
 app.get('/', function (req, res, next) {
     res.sendFile(__dirname + '/index.html');
 });
 
 server.listen(4200);
+
+io.on('connection', function(client) {  
+    console.log('Client connected...');
+
+    client.on('join', function(data) {
+        console.log(data);
+        client.emit('messages', 'Hello from server');
+    });
+});
