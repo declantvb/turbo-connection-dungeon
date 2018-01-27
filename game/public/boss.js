994fdb6a27caa1d15d0b6e4da3083f3ba6ef63b1
Jack Haystead <jackhaystead@gmail.com>
2018-01-27 14:57:41 +1300

    Did stuff to boss

--- game/public/boss.js
+++ game/public/boss.js
@@ -1,22 +1,40 @@
-// Ohhh shiiititttt
-
-
 var Boss = function () {
+    let self = this;
 
     this.sprites = [];
-    this.sprites.push(game.add.sprite(0, 0, 'body'));
-    this.sprites.push(game.add.sprite(0, 0, 'head'));
-    this.sprites.push(game.add.sprite(0, 0, 'leg'));
+    addSprite('body');
+    addSprite('head');
+    addSprite('leg', -50, -10);
+    addSprite('leg', -50, 0);
+    addSprite('leg', -50, 10);
+    addSprite('leg', 50, -10);
+    addSprite('leg', 50, 0);
+    addSprite('leg', 50, 10);
+    
+    function addSprite(name, xOffset, yOffset){
+        let sprite = game.add.sprite(0, 0, name);
+        sprite.anchor.setTo(0.5, 1);
+        spriteGroup.add(sprite);
+
+        self.sprites.push({
+            sprite: sprite,
+            xOffset: xOffset || 0,
+            yOffset: yOffset || 0
+        });
+    }
 
     this.scale(0.15);
 
     console.log('Boss made');
 }
 
+
+
 Boss.prototype.move = function (x, y) {
     for (var i = 0; i < this.sprites.length; i++) {
-        this.sprites[i].x = x;
-        this.sprites[i].y = y;
+        let sprite = this.sprites[i];
+        sprite.x = x + sprite.xOffset;
+        sprite.y = y + sprite.yOffset;
     }
 
     this.x = x;
@@ -25,8 +43,8 @@ Boss.prototype.move = function (x, y) {
   
 Boss.prototype.scale = function (s) {
     for (var i = 0; i < this.sprites.length; i++) {
-        this.sprites[i].scale.x = s;
-        this.sprites[i].scale.y = s;
+        this.sprites[i].sprite.scale.x = s;
+        this.sprites[i].sprite.scale.y = s;
     }
 
     this.width = this.width * s;
